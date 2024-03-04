03b97c87ae7679946163c92fd71c0aff65fb500e
Annatina Christ <annatina.christ@espas.ch>
2024-03-04 09:54:01 +0000

    lists, nested lists etc

--- main.py
+++ main.py
@@ -1,25 +1,44 @@
 #import the random module
-import random
+#import random
 #import my_module
 
-
 #generate a random integer
-random_integer = random.randint(1, 10)
+#random_integer = random.randint(1, 10)
 
 #generate a random float without a range (0.000000 - 0.999999)
 #get whole numbers by multiplying the float by 5
 # e.g. random_float * 5
 
-random_float = random.random() * 5
+#random_float = random.random() * 5
 
-love_score = random.randint(1, 100)
-
-
-
-
+#love_score = random.randint(1, 100)
 
 
 #print(my_module.pi)
 #print(random_integer)
 #print(random_float)
-print(f"Your love score is {love_score}")
+#print(f"Your love score is {love_score}")
+
+#generate a list
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+#go through a list in the order it was saved in
+#positive offset starts at begining of list
+
+
+#print(states_of_america)
+
+
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen[1][1])
+
+
+
+
+
+
